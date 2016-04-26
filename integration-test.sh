f2f2eb919e26b87085af7c635010b1db56218911
dexter <dgenterone@acale.ph>
2016-04-26 16:27:34 +0800

    added integration test script

---
+++ integration-test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+test() {
+	echo 'Running Integration Test...'
+	local url="$1"
+
+	pong=$(wget -q -O - "${url}/ping")
+
+	if [[ "$?" != "0" ]]; then 
+		echo "Error connecting to pingpong server."
+		return 1
+	fi
+
+	if [[ "${pong}" != "pong" ]]; then
+		echo "didn't receive the correct response."
+		return 1
+	fi
+
+	echo "Test successful."
+	return 0		
+}
+
+test $@
